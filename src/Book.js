b869d44d5fdae905ab76e7fe2c54886699ec3b04
Peter O'Connor <poc275@gmail.com>
2021-08-15 14:55:32 +0100

    Adds new Airtable base.

--- src/Book.js
+++ src/Book.js
@@ -30,7 +30,8 @@ class Book extends Component {
             new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('app2XCAIl9QhJYPkr'),
             new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appwADT4du02NZBMU'),
             new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appm6jVVBaS1DeQJC'),
-            new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appklQU840y2TrAy8')
+            new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appklQU840y2TrAy8'),
+            new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('app92pxeMLKnwiZrQ')
         ];
         
         const promises = bases.map(base => {
